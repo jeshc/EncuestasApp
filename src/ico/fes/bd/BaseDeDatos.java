be19cc016ce23773ad5fa5a5873c769334125be3
jeshc <hcjesus@gmail.com>
2022-03-07 17:59:14 -0500

    Inicial

---
+++ src/ico/fes/bd/BaseDeDatos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.bd;
+
+/**
+ *
+ * @author unam
+ */
+public class BaseDeDatos {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Probando conexión BD..");
+        
+        
+    }
+}
